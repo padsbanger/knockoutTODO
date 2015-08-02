12f9f927c528858e0dd4b10eb2f77bc2b69daede
Michał Lach <kontakt@michal-lach.pl>
2015-08-02 17:54:31 +0200

    filter TODO, set as done

--- app.js
+++ app.js
@@ -1,28 +1,53 @@
 function Task(data) {
     this.title = ko.observable(data.title)
+    this.isDone = ko.observable();
 }
 
 function TaskListViewModel() {
     var self = this;
+
+    self.search = ko.observable('');
     self.newTaskText = ko.observable();
     self.tasks = ko.observableArray([{
-        title: 'Buy milk'
+        title: 'Buy milk',
+        isDone : ko.observable(false)
     }, {
-        title: 'Feed a cat'
+        title: 'Feed a cat',
+        isDone: ko.observable(false)
     }]);
 
-    self.addTask = function() {
-        if (self.newTaskText.length > 0) {
-            self.tasks.push(new Task({
-                title: self.newTaskText()
-            }))
-            self.newTaskText('');
+    self.filter = ko.computed(function() {
+        var lower = self.search().toLowerCase();
+        if (!lower) {
+            return self.tasks();
+        } else {
+            return ko.utils.arrayFilter(self.tasks(), function(task) {
+                return task.title.toLowerCase().indexOf(lower) !== -1;
+            })
         }
+    });
 
+    self.totalTODOCount = ko.computed(function() {
+        return self.tasks().length;
+    });
+
+    self.incompleteTasks = ko.computed(function() {
+        return ko.utils.arrayFilter(self.tasks(), function(task) { return !task.isDone() });
+    });
+
+
+    self.addTask = function() {
+        self.tasks.push(new Task({
+            title: self.newTaskText(),
+            isDone: false
+        }))
+        self.newTaskText('');
     }
     self.removeTask = function(task) {
-      self.tasks.remove(task)
+        self.tasks.remove(task)
     }
+
+
 }
 
 ko.applyBindings(new TaskListViewModel());
